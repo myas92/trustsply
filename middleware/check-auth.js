c24684383bac3e8ff92486f58b4e2ccdaa4c2b1e
Yaser Ahmadi <m.y.ahmadi22@gmail.com>
2021-04-15 16:42:37 +0430

    Editing user info (just name )

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,21 +1,33 @@
-const jwt = require('jsonwebtoken');
-
-const HttpError = require('../models/http-error');
-
-module.exports = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const HttpError = require("../models/http-error");
+const Errors = require("../const/errors");
+const dbConfig = require("../initializer");
+const redisController = require("../util/redis-controller");
+module.exports = async (req, res, next) => {
+  if (req.method === "OPTIONS") {
     return next();
   }
   try {
-    const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
     if (!token) {
-      throw new Error('Authentication failed!');
+      const error = new HttpError(Errors.Authentication_Failed, req.language);
+      return next(error);
     }
-    const decodedToken = jwt.verify(token, 'supersecret_dont_share');
+    const decodedToken = jwt.verify(token, config.JWT);
+    let identifiedToken = await redisController.hget(
+      "verifyCodes",
+      decodedToken.userId,
+      token
+    );
+    if (!identifiedToken) {
+      const error =new HttpError(Errors.Authentication_Failed, req.language);
+      return next(error);
+    }
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
-    const error = new HttpError('Authentication failed!', 403);
+    const error = new HttpError(Errors.Authentication_Failed, req.language);
     return next(error);
   }
 };
--- services/users.js
+++ services/users.js
@@ -21,10 +21,33 @@ async function getUserByEmail(email) {
 		throw error;
 	}
 }
+async function getUserById(usedId) {
+	try {
+		let sql = `SELECT * FROM users WHERE id = '${usedId}' AND isDeleted = false`;
+		const data = await db.query(sql);
+		return {
+			data
+		};
+	} catch (error) {
+		throw error;
+	}
+}
+async function updateUser(input) {
+	try {
+		let { userId, name } = input;
+		let sql = `UPDATE users SET name = '${name}' WHERE id = '${userId}'`;
+		const data = await db.query(sql);
+		return {
+			data
+		};
+	} catch (error) {
+		throw error;
+	}
+}
 async function ConfirmUser(input) {
 	const connection = await db.connection();
 	try {
-		let userId = uuid()
+		let userId = uuid();
 		await connection.query('START TRANSACTION');
 		let { verifyUserId, name, email, password } = input;
 		let sqlUsers = `INSERT INTO users (id, name, email, password)
@@ -33,7 +56,7 @@ async function ConfirmUser(input) {
 		let sqlVerifyUser = `UPDATE verify_users SET isUsed = true WHERE id = '${verifyUserId}'`;
 		await connection.query(sqlVerifyUser);
 		await connection.query('COMMIT');
-		return {userId};
+		return { userId };
 	} catch (error) {
 		await connection.query('ROLLBACK');
 		console.log('ROLLBACK at querySignUp', error);
@@ -46,6 +69,7 @@ async function ConfirmUser(input) {
 module.exports = {
 	getUsers,
 	getUserByEmail,
-	ConfirmUser
+	ConfirmUser,
+	getUserById,
+	updateUser
 };
-
